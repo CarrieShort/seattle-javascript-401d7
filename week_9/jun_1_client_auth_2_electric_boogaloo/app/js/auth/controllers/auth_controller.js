3c22fde6bdcd4892ce1458ced0e0902e5cee05aa
CarrieShort <carrie.r.phillips@gmail.com>
2016-06-05 19:52:25 -0700

    comment on auth_controller

--- week_9/jun_1_client_auth_2_electric_boogaloo/app/js/auth/controllers/auth_controller.js
+++ week_9/jun_1_client_auth_2_electric_boogaloo/app/js/auth/controllers/auth_controller.js
@@ -4,6 +4,7 @@ module.exports = function(app) {
     this.errors = [];
     this.getUsername = function() {
       // AUTH_EXP: What happens when this function is called?
+      // calls the cfAuth service to get username, which returns a promise. If resolved it will return a username which will be stored as username on the authController. If the promise is rejected then it will call the handleError service with the error message 'could not get username'
       auth.getUsername()
         .then((currentUser) => {
           this.username = currentUser;
