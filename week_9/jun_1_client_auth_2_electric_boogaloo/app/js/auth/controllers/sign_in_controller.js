459ca61bece2b685671cb91db76a2d7da2362e5a
CarrieShort <carrie.r.phillips@gmail.com>
2016-06-05 19:47:33 -0700

    differences between signin and signup controllers

--- week_9/jun_1_client_auth_2_electric_boogaloo/app/js/auth/controllers/sign_in_controller.js
+++ week_9/jun_1_client_auth_2_electric_boogaloo/app/js/auth/controllers/sign_in_controller.js
@@ -2,6 +2,10 @@ var baseUrl = require('../../config').baseUrl;
 module.exports = function(app) {
   app.controller('SignInController', ['$http', '$location', 'cfHandleError', 'cfAuth', function($http, $location, handleError, auth) {
     // AUTH_EXP: how does this differ from the sign_up_controller?
+    // no this.signup which will change how the auth_view renders the template.
+    // this.buttonText is Sign in to existing user which changes the button text in auth_view.
+    // this.authenticate is a GET request ot api/signin. And passes the headers username and password using basic authorization.
+    // the response is mostly the same, but the handlError service is given a different error message.
     this.buttonText = 'Sign in to existing user';
     this.errors = [];
     this.authenticate = function(user) {
--- week_9/jun_1_client_auth_2_electric_boogaloo/app/js/auth/controllers/sign_up_controller.js
+++ week_9/jun_1_client_auth_2_electric_boogaloo/app/js/auth/controllers/sign_up_controller.js
@@ -2,6 +2,10 @@ var baseUrl = require('../../config').baseUrl;
 module.exports = function(app) {
   app.controller('SignUpController', ['$http', '$location',  'cfHandleError', 'cfAuth', function($http, $location, handleError, auth) {
     // AUTH_EXP: how does this differ from the sign_in_controller
+    // signup is true which will change how the auth_view renders the template.
+    // this.buttonText is Create New User which changes the button text in auth_view.
+    // this.authenticate posts to api/signup
+    // the response is mostly the same, but the handlError service is given a different error message.
     this.signup = true;
     this.errors = [];
     this.buttonText = 'Create New User!'
